74e1a30ce6994b20a75e5ed49c88c894e2ea238e
Joseph Kao <josephtkao@gmail.com>
2020-04-14 03:36:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,19 @@
 # code here!
 class School
   roster = {}
+  def initialize(name)
+    @name=name
+  end
+  def add_student(student,grade)
+    roster(grade => student)
+  end
+
+  def grade(num)
+    roster[num]
+  end
+
+  def sorted
+    roster.each do |grade, student|
+      grade.values.sort
+    end
 end
